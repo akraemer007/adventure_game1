fc587486ea0a3c42bbc0122f84783e10ae2aca53
kidman007 <kidman007@gmail.com>
2019-06-02 09:43:23 -0500

    wrapped player and movement in class

--- engine.py
+++ engine.py
@@ -1,16 +1,22 @@
 import tcod as libtcod
+from input_handlers import handle_keys
+from entity import Entity
+from render_all import render_all, clear_all
 
 
 def main():
     screen_width = 80
     screen_height = 50
 
-    player_x = int(screen_width / 2)
-    player_y = int(screen_height / 2)
+    player = Entity(int(screen_width / 2), int(screen_height / 2), '@', libtcod.white)
+    npc = Entity(int(screen_width / 2), int(screen_height / 2), '@', libtcod.yellow)
+    entities = [player, npc]
 
     libtcod.console_set_custom_font('arial10x10.png', libtcod.FONT_TYPE_GREYSCALE | libtcod.FONT_LAYOUT_TCOD)
 
     libtcod.console_init_root(screen_width, screen_height, 'libtcod tutorial revised', False)
+
+    con = libtcod.console_new(screen_width, screen_height)
 
     key = libtcod.Key()
     mouse = libtcod.Mouse()
@@ -18,14 +24,27 @@ def main():
     while not libtcod.console_is_window_closed():
         libtcod.sys_check_for_event(libtcod.EVENT_KEY_PRESS, key, mouse)
 
-        libtcod.console_set_default_foreground(0, libtcod.white)
-        libtcod.console_put_char(0, player_x, player_y, '@', libtcod.BKGND_NONE)
+        render_all(con, entities, screen_width, screen_height)
+
         libtcod.console_flush()
 
-        key = libtcod.console_check_for_keypress()
+        clear_all(con, entities)
 
-        if key.vk == libtcod.KEY_ESCAPE:
+        action = handle_keys(key)
+
+        move = action.get('move')
+        exit = action.get('exit')
+        fullscreen = action.get('fullscreen')
+
+        if move:
+            dx, dy = move
+            player.move(dx, dy)
+
+        if exit:
             return True
+
+        if fullscreen:
+            libtcod.console_set_fullscreen(not libtcod.console_is_fullscreen())
 
 if __name__ == '__main__':
     main()
